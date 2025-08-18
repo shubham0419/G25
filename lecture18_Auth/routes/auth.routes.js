eef7d563a15631b50b643d6c2f42b11e95a8f1d5
shubham0419 <shubhamsc704707@gmail.com>
2025-08-18 14:12:53 +0530

    jwt

--- lecture18_Auth/routes/auth.routes.js
+++ lecture18_Auth/routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const User = require("../models/user.model");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const verifyUser = require("../middleware/verify.middleware");
 
 router.post("/signup",async (req,res)=>{
   try {
@@ -33,11 +35,35 @@ router.post("/login",async (req,res)=>{
     if(!isMatched){
       throw new Error("Invalid email or password")
     }
-    res.status(200).json({message:"user loggedin successfull"})
+    const token = jwt.sign({id:user._id,name:user.name,email:user.email},
+      process.env.JWT_SECRET,{expiresIn:'1h',algorithm:"HS256"}
+    )
+    res.status(200).json({message:"user loggedin successfull",token:token})
   } catch (error) {
     res.status(400).json({message:error.message})
   }
 })
 
+// can access anyone
+// router.get("/check",async (req,res)=>{
+//   try {
+//     res.status(200).json({message:"welcome user"})
+//   } catch (error) {
+//     res.status(400).json({message:error.message});
+//   }
+// })
+
+// logged in user only
+router.get("/check",verifyUser,async (req,res)=>{
+  try {
+    // const authorization = req.headers.authorization;
+    // const token = authorization.split(" ")[1];
+    // const payload = jwt.verify(token,process.env.JWT_SECRET)
+    // res.status(200).json({message:"welcome user",user:payload})
+    res.status(200).json({message:"welcome user",user:req.user})
+  } catch (error) {
+    res.status(400).json({message:error.message});
+  }
+})
 module.exports = router;
 
