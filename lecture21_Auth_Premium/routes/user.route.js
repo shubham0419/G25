0041f9077e1e57648dfb58a059a5a17aa6e5bd51
shubham0419 <shubhamsc704707@gmail.com>
2025-08-29 11:02:19 +0530

    ejs

---
+++ lecture21_Auth_Premium/routes/user.route.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const verifyUser = require("../middleware/verify.middleware");
+const User = require("../models/user.model");
+const Product = require("../models/product.model");
+const { verifyGoldUser, verifyPlatinumUser } = require("../middleware/verify.premium.middleware");
+const router = express.Router();
+
+const products = [
+  {
+    name:"product 1",
+    price:1999,
+    description:"this is product 1"
+  },
+  {
+    name:"product 2",
+    price:999,
+    description:"this is product 2"
+  },
+  {
+    name:"product 3",
+    price:1499,
+    description:"this is product 3"
+  },
+  {
+    name:"product 4",
+    price:1799,
+    description:"this is product 4"
+  },
+]
+
+router.get("/create/products",async(req,res)=>{
+  try {
+    const allProducts = await Product.insertMany(products);
+    res.status(200).json({products:allProducts})
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
+
+router.post("/package/buy",verifyUser,async(req,res)=>{
+  try {
+    const {package} = req.query;
+    if(!package){
+      throw new Error("package name is required")
+    }
+    const currUserId = req.user.id;
+    const user = await User.findById(currUserId);
+    user.package = package;
+    if(package=="gold"){
+      user.credits += 500;
+    }
+    if(package=="platinum"){
+      user.credits += 1000;
+    }
+    await user.save();
+    res.status(200).json({message:"package bought successfully"})
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
+
+
+router.get("/gold/discount/:productId",verifyUser,verifyGoldUser,async(req,res)=>{
+  try {
+    const {productId} = req.params;
+    const userId = req.user.id;
+
+    // const user = await User.findById(userId)
+    // const product = await Product.findById(productId)
+
+    const [user,product] = await Promise.all([
+      await User.findById(userId),
+      await Product.findById(productId)
+    ]);
+
+    if(user.credits - (product.price*0.1)<0){
+      throw new Error("Not enough credits")
+    }
+
+    const discountedPrice = product.price - (product.price*0.1);
+    user.credits -= (product.price*0.1);
+    await user.save();
+    res.status(200).json({newPrice:discountedPrice});
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
+
+
+router.get("/platinum/discount/:productId",verifyUser,verifyPlatinumUser,async(req,res)=>{
+  try {
+    const {productId} = req.params;
+    const userId = req.user.id;
+
+    const [user,product] = await Promise.all([
+      await User.findById(userId),
+      await Product.findById(productId)
+    ]);
+
+    if(user.credits - (product.price*0.2)<0){
+      throw new Error("Not enough credits")
+    }
+    
+    const discountedPrice = product.price - (product.price*0.2);
+    user.credits -= (product.price*0.2);
+    await user.save();
+    res.status(200).json({newPrice:discountedPrice});
+  } catch (error) {
+    res.status(500).json({message:error.message})
+  }
+})
+module.exports = router;
--- lecture22_EJS_Auth/index.js
+++ lecture22_EJS_Auth/index.js
@@ -4,21 +4,26 @@ const app = express();
 const path = require("path");
 const PORT = 5000;
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
 // router
 const authRouter = require("./routes/auth.routes");
-const userRouter = require("./routes/user.route")
+const userRouter = require("./routes/user.route");
+const verifyUser = require("./middleware/verify.middleware");
 
+app.use(cookieParser());
 app.set("view engine","ejs");
 app.set('views',path.join(__dirname,"views"))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-  res.render()
+app.get("/",verifyUser,(req,res)=>{
+  const name = "shubham";
+  const contacts = [{name:"Contact 1",phone:9328469324},
+    {name:"Contact 2",phone:8758757656}]
+  res.render("hello",{name:name,contacts})
   // upper line is same as bellow
   // res.render("hello",{username:name,contacts:contacts})
 })
-
 // routes
 app.use("/auth",authRouter);
 app.use("/user",userRouter);
