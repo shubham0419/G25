b61df03853e50bab0c7e815cdb27cc8b05c398db
shubham0419 <shubham.e21@nsut.ac.in>
2025-11-07 10:22:14 +0530

    noticefication system

--- lecture36_noticefication_system/backend/index.js
+++ lecture36_noticefication_system/backend/index.js
@@ -4,42 +4,89 @@ const http = require("http");
 const cors = require("cors");
 const server = http.createServer(app);
 const socket = require("socket.io");
+const {v4:uuid} = require("uuid");
 const io = socket(server,{
   cors:{
     origin:"http://localhost:3000",
   }
 });
-const PORT = 5000;
+const PORT = 4000;
 const path = require("path");
 
-app.use(cors({
-  origin:"http://localhost:3000",
-  methods:"POST, GET"
-}))
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,"public")))
 
+//{ 
+// "username":"socket.id"
+// "username2":socket.id
+// }
 const Users = {};
 
 //  post - > {
-  // author  -> user
+  // id.  ->.  
+  // author  -> username
   // content  ->string
-  // likes -> [user]
-  // createdAt -> date 
+  // likes -> [username]
+  // createdAt -> date
 // }
-const posts = []
+let Posts = []
 
 io.on("connection",(client)=>{
   console.log("User 1 connected -> ",client.id);
   
-  client.on("register",(userName)=>{
-    Users[userName] = client.id
+  // register user
+  client.on("register",(username)=>{
+    Users[username] = socket.id
   })
+  
+})
+
+app.post("/post/create",async (req,res)=>{
+  try {
+    const {username,content} = req.body;
+    const post = {
+      id:uuid(),
+      author:username,
+      content,
+      likes:[],
+      createdAt: new Date()
+    }
+    Posts.unshift(post);
+    res.status(201).json({posts:Posts})
+  } catch (error) {
+    res.status(401).json({message:error.message})
+  }
+})
+
+app.get("/post/all",async (req,res)=>{
+  res.status(200).json({posts:Posts})
+})
+
+app.post("/post/like/:id/:username",(req,res)=>{
+  try {
+    const {id,username} = req.params;
+    // [post,post,updated post, post ,post]
+    Posts = Posts.map((post)=>{
+      if(post.id == id ){
+        if(post.likes.includes(username)){
+          throw new Error("alreday liked the post");
+        }
+        post.likes.push(username);
+      }
+      return post;
+    })
+    res.status(200).json({message:"post updated successfully"})
+  } catch (error) {
+    res.status(401).json({message:error.message})
+  }
 })
 
 app.get("/", (req, res) => {
   res.send("server running");
 });
+
+
 
 server.listen(PORT, () => console.log("Server running on port " + PORT));
--- lecture36_noticefication_system/frontend/src/app/page.js
+++ lecture36_noticefication_system/frontend/src/app/page.js
@@ -1,16 +1,34 @@
 "use client"
+import axios from "axios";
+import { Heart, User } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import {io} from "socket.io-client"
 
 export default function Home() {
   const [socket,setSocket] = useState(null);
+  const [isLoggenIn,setIsLoggedIn] = useState(false);
+  const [username,setUsername] = useState("");
+  const [content,setContent] = useState("");
+  const [posts,setPosts] = useState([]);
+  const [refresh,setRefresh] = useState(0);
 
   // socket client initialise
   useEffect(()=>{
-    const newSocket = io("http://localhost:5000");
+    const newSocket = io("http://localhost:4000");
     setSocket(newSocket);
   },[])
+
+  const gettAllPosts = async ()=>{
+    if(isLoggenIn){
+      let res = await axios.get("http://localhost:4000/post/all");
+      setPosts(res.data.posts);
+    }
+  }
+
+  useEffect(()=>{
+    gettAllPosts();
+  },[refresh])
 
   // client connection
   useEffect(()=>{
@@ -19,65 +37,74 @@ export default function Home() {
     })
   },[socket])
 
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    socket.emit("register",username);
+    setIsLoggedIn(true);
+    setRefresh(prev=>prev+1);
+  }
+
+
+
+  if(!isLoggenIn){
+    return (
+      <div className="h-screen w-full bg-white text-black flex justify-center items-center">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 border rounded-md p-5">
+          <label form="username">Username</label>
+          <input onChange={(e)=>setUsername(e.target.value)} className="border" id="username" placeholder="Enter Name"/>
+          <button className="border bg-blue-300 rounded-lg">Register</button>
+        </form>
+      </div>
+    )
+  }
+
+  const handlePostCreate = async (e)=>{
+    e.preventDefault();
+    let payload = {
+      username,
+      content
+    }
+    let res = await axios.post("http://localhost:4000/post/create",payload);
+    if(res.status==201){
+      setRefresh(prev=>prev+1);
+    }
+  }
+
+  const handlePostLike = async (id)=>{
+    try {
+      let res = await axios.post(`http://localhost:4000/post/like/${id}/${username}`);
+      if(res.status==200){
+        setRefresh(prev => prev+1);
+      } 
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="min-h-screen w-full bg-white text-black px-20 py-10">
+      <h1 className="text-2xl font-semibold">Hello {username}!!</h1>
+
+      <form onSubmit={handlePostCreate} className="flex flex-col gap-2 border rounded-md p-5">
+          <label form="username">Create Tweet</label>
+          <textarea rows={3} cols={7} onChange={(e)=>setContent(e.target.value)} className="border" id="username" placeholder="Enter Name"/>
+          <button className="border bg-blue-300 rounded-lg">Post</button>
+        </form>
+
+      <div className="p-5 flex flex-col gap-3">
+          {posts?.map((post,indx)=>{
+            return <div key={indx} className="p-4 border rounded-lg shadow-md">
+            <div className="flex gap-2 items-center">
+              <User className="h-7 w-7 border rounded-full"/>
+              <h4 className="text-lg font-semibold">{post.author}</h4>
+            </div>
+              <p className="text-xl">{post.content}</p>
+              <p className="text-sm text-gray-400 float-end">{new Date(post.createdAt).toLocaleString()}</p>
+              <button disabled={post.likes.includes(username)} onClick={()=>handlePostLike(post.id)} className={`flex ${post.likes.includes(username)?"opacity-45 cursor-not-allowed":""}`}>{post.likes.includes(username)?<Heart fill="red" className="mr-2"/>
+              :<Heart className="mr-2"/>} {post.likes.length} Likes</button>
+            </div>
+          })}
+      </div>
     </div>
   );
 }
